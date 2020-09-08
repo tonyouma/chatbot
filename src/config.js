0b4428aec72510659fe58629517f02c9e9213bce
TonyOuma <kyrietony@gmail.com>
2020-09-08 17:50:08 +0300

    feat: add another property to the  widget object in the config(to dynamically pass in props to the component

--- src/config.js
+++ src/config.js
@@ -17,11 +17,32 @@ const config = {
             },
             {
                 widgetName: "javacriptLinks",
-                widgetFunc: (props) => <LinkList {...props} />
-            }
-        ]
+                widgetFunc: (props) => <LinkList {...props} />,
+                
+                props: {
+                    options: [
+                        {
+                        text: "Introduction to JS",
+                        url: "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/",
+                        id: 1,
+                        },
+                        {
+                            text: "Mozilla JS Guide",
+                            url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide",
+                            id: 2,
+                        },
+                        {
+                            text: "Frontend Masters",
+                            url: "https://frontendmasters.com",
+                            id: 3,
+                        },
+                       
+                    ],
+                },
+            },
+        ],
     
     
-}
+};
 
 export default config;
