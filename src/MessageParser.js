2892af928cf1aad900309b0317133c5d081164e0
TonyOuma <kyrietony@gmail.com>
2020-09-09 00:56:49 +0300

    feat: update MessageParser to allow users use the input field

--- src/MessageParser.js
+++ src/MessageParser.js
@@ -9,6 +9,9 @@ class MessageParser {
         if ( lowerCaseMessage.includes("hello")) {
             this.actionProvider.greet()
         }
+        if ( lowerCaseMessage.includes("javascript")) {
+            this.actionProvider.handleJavascriptList();
+        }
     }
 }
 
