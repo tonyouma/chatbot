66b723da5bb59a348b51eff8c159d70a6d3a8097
TonyOuma <kyrietony@gmail.com>
2020-09-08 14:17:29 +0300

    feat: modify MessageParser to receive user message

--- src/MessageParser.js
+++ src/MessageParser.js
@@ -1,11 +1,14 @@
 class MessageParser {
     constructor( actionProvider, state) {
         this.actionProvider = actionProvider;
-        this.state = state;
     }
 
     parse(message){
-        console.log(message);
+        const lowerCaseMessage = message.toLowerCase()
+
+        if ( lowerCaseMessage.includes("hello")) {
+            this.actionProvider.greet()
+        }
     }
 }
 
