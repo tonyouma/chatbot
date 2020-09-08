290c3bd148e92a330e9ae81b78b9bf46fd133f63
TonyOuma <kyrietony@gmail.com>
2020-09-08 13:56:56 +0300

    Updates

---
+++ precache-manifest.fc280dfde5adb262bdef937e4c0e57eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5350ba51f5393efb33142cd5ac5d06c",
+    "url": "/chatbot/index.html"
+  },
+  {
+    "revision": "76bc962e440de2c58af2",
+    "url": "/chatbot/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "69b76d2d399ae47a568b",
+    "url": "/chatbot/static/js/2.b86b7e29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chatbot/static/js/2.b86b7e29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76bc962e440de2c58af2",
+    "url": "/chatbot/static/js/main.5f9286ef.chunk.js"
+  },
+  {
+    "revision": "6da786bfa225ae5459b9",
+    "url": "/chatbot/static/js/runtime-main.253fb56a.js"
+  }
+]);
